e570157c9c18341091325827e6b7328e0e16055e
ycomlee <ycomlee@gmail.com>
2026-04-15 15:27:37 +0900

    Add src/components/Intro.jsx

---
+++ src/components/Intro.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <section id="intro" className="intro-section">
+      <div className="spline-container">
+        <iframe 
+          src='https://my.spline.design/cutecomputerfollowcursor-vtZGRdv3ngYJfoFdojl5WpDv/'
+          //트래킹 https://my.spline.design/cutecomputerfollowcursor-vtZGRdv3ngYJfoFdojl5WpDv/
+          //노트북 https://my.spline.design/interactiveworkplace-Y3Si77Fn1GJ3wNiKfbkE2erG/
+          frameBorder="0" 
+          width="100%" 
+          height="100%" 
+          title="Spline 3D Background"
+        ></iframe>
+      </div>
+      <div className="intro-content">
+        <h1 className="intro-title">길동의 포트폴리오</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
